1edcd76df86086a226656a47ca1cec6a42befe26
Adolfo <adolfo.luna@virginiogomez.cl>
2022-09-30 21:09:40 -0300

    cambio de barcode scanner + aproximacion inicial de creacion de pdf en base a lista de codigos

---
+++ app/src/main/java/com/example/pic/AnyOrientationCaptureActivity.java
@@ -0,0 +1,11 @@
+package com.example.pic;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * This Activity is exactly the same as CaptureActivity, but has a different orientation
+ * setting in AndroidManifest.xml.
+ */
+public class AnyOrientationCaptureActivity extends CaptureActivity {
+
+}
--- app/src/main/java/com/example/pic/MainActivity.java
+++ app/src/main/java/com/example/pic/MainActivity.java
@@ -1,99 +1,72 @@
 package com.example.pic;
 
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import com.budiyev.android.codescanner.CodeScanner;
-import com.budiyev.android.codescanner.CodeScannerView;
-import com.budiyev.android.codescanner.DecodeCallback;
-import com.budiyev.android.codescanner.ScanMode;
-import com.google.zxing.Result;
+import com.google.zxing.client.android.Intents;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private CodeScanner mCodeScanner;
-    Button button;
+    ListView listView;
+    List<String> list;
+    Button pdfGenerator;
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if (result.getContents() == null) {
+                    Intent originalIntent = result.getOriginalIntent();
+                    if (originalIntent == null) {
+                        Log.d("MainActivity", "Cancelled scan");
+                        Toast.makeText(MainActivity.this, "Cancellado", Toast.LENGTH_LONG).show();
+                    } else if (originalIntent.hasExtra(Intents.Scan.MISSING_CAMERA_PERMISSION)) {
+                        Log.d("MainActivity", "Cancelled scan due to missing camera permission");
+                        Toast.makeText(MainActivity.this, "Scan cancelado por falta de permiso de camara", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Log.d("MainActivity", "Scanned");
+                    Toast.makeText(MainActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                    list.add(result.getContents());
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list));
+        pdfGenerator = findViewById(R.id.pdfGenerationButton);
+        pdfGenerator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                cameraPermission.launch(Manifest.permission.CAMERA);
-            }
-        });
-        cameraPermission.launch(Manifest.permission.CAMERA);
-        CodeScannerView scannerView = findViewById(R.id.scanner_view);
-        mCodeScanner = new CodeScanner(this, scannerView);
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
-            @Override
-            public void onDecoded(@NonNull final Result result) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-        scannerView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCodeScanner.startPreview();
+                PdfDocument document = new PdfDocument();
+                PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(100, 100, 1).create();
+                PdfDocument.Page page = document.startPage(pageInfo);
+                View content = view;
+                content.draw(page.getCanvas());
             }
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mCodeScanner.startPreview();
-    }
-
-    @Override
-    protected void onPause() {
-        mCodeScanner.releaseResources();
-        super.onPause();
-    }
-    ActivityResultLauncher<String> cameraPermission = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
-        @Override
-        public void onActivityResult(Boolean result) {
-            if (result) {
-                Toast.makeText(getApplicationContext(),"Camera Permission Granted", Toast.LENGTH_SHORT).show();
-            } else
-            {
-                Toast.makeText(getApplicationContext(),"Camera Permission not Granted", Toast.LENGTH_SHORT).show();
-            }
-        }
-    });
-    private void generatePDF (){
-        PdfDocument pdfDocument = new PdfDocument();
-        Paint title = new Paint();
-        PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(1120,792,1).create();
-        PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
-        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
-        title.setTextSize(16);
-        title.setColor(ContextCompat.getColor(this,R.color.purple_200));
-
+    public void scanBarcodeCustomLayout(View view) {
+        ScanOptions options = new ScanOptions();
+        options.setCaptureActivity(AnyOrientationCaptureActivity.class);
+        options.setDesiredBarcodeFormats(ScanOptions.ONE_D_CODE_TYPES);
+        options.setPrompt("Escanea algo");
+        options.setOrientationLocked(true);
+        options.setBeepEnabled(false);
+        barcodeLauncher.launch(options);
     }
 }
