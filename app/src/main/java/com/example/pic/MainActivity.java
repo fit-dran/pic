700b6c2009533b7072aa64f7e2fd1d29359ecf9f
Adolfo <adolfo.luna@virginiogomez.cl>
2022-10-04 21:47:58 -0300

    creacion de pdf en base a list array de codigo de barras implementada

--- app/src/main/java/com/example/pic/MainActivity.java
+++ app/src/main/java/com/example/pic/MainActivity.java
@@ -4,8 +4,8 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -14,14 +14,23 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.zxing.client.android.Intents;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-    List<String> list;
+    List<String> list = new ArrayList<String>();
     Button pdfGenerator;
     private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
             result -> {
@@ -45,19 +54,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.listView);
-        listView.setAdapter(new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list));
-        pdfGenerator = findViewById(R.id.pdfGenerationButton);
-        pdfGenerator.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PdfDocument document = new PdfDocument();
-                PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(100, 100, 1).create();
-                PdfDocument.Page page = document.startPage(pageInfo);
-                View content = view;
-                content.draw(page.getCanvas());
-            }
-        });
+       listView = findViewById(R.id.listView);
+listView.setAdapter(new ArrayAdapter<String>(this, androidx.appcompat.R.layout.abc_screen_simple, list));
+
+pdfGenerator = findViewById(R.id.pdfGenerationButton);
+pdfGenerator.setOnClickListener(new View.OnClickListener() {
+          @Override
+           public void onClick(View view) {
+               try {
+                  createPdf(list);
+              } catch (Exception e){
+                   Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_SHORT).show();
+               }
+         }
+    });
     }
 
     public void scanBarcodeCustomLayout(View view) {
@@ -68,5 +78,22 @@ public class MainActivity extends AppCompatActivity {
         options.setOrientationLocked(true);
         options.setBeepEnabled(false);
         barcodeLauncher.launch(options);
+    }
+    private void createPdf(List<String> input) throws FileNotFoundException{
+        String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+        File file = new File(pdfPath,"myPDF.pdf");
+        
+        OutputStream outputStream = new FileOutputStream(file);
+
+        PdfWriter writer = new PdfWriter(file);
+        PdfDocument pdfDocument = new PdfDocument(writer);
+        Document document = new Document(pdfDocument);
+        input.forEach((n)->createParagraph(n,document));
+        document.close();
+        Toast.makeText(this, "Pdf Created", Toast.LENGTH_SHORT).show();
+    }
+    private void createParagraph (String input , Document document){
+        Paragraph paragraph = new Paragraph(input);
+        document.add(paragraph);
     }
 }
